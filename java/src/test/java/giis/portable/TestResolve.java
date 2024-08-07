50dbf441e26a99b29d807f258d288cc23ec9b2f5
Javier <10879637+javiertuya@users.noreply.github.com>
2024-08-07 09:52:52 +0200

    Test path resolution

---
+++ java/src/test/java/giis/portable/TestResolve.java
@@ -0,0 +1,72 @@
+package giis.portable;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import giis.portable.util.FileUtil;
+import giis.portable.util.JavaCs;
+
+public class TestResolve {
+
+	// More tests on resolution of the source file location
+	@Test
+	public void testSourcePathResolution() {
+		assertPath("/a/b/c/x/y/Clazz.java", "/a/b/c", "", "x/y/Clazz.java");
+		assertPath("a/b/c/x/y/Clazz.java", "a/b/c", "", "x/y/Clazz.java");
+		assertPath("a/b/c/x/y/Clazz.java", "a/b/c/", "", "x/y/Clazz.java");
+		assertPath("a/b/c/x/y/Clazz.java", "a\\b\\c", "", "x\\y\\Clazz.java");
+		assertPath("c:/a/b/c/x/y/Clazz.java", "c:\\a\\b\\c", "", "x\\y\\Clazz.java");
+		
+		// if no source folder or file, returns empty (no resolved)
+		assertPath("", "", "", "x/y/Clazz.java");
+		assertPath("", null, "", "x/y/Clazz.java");
+		assertPath("", "", null, "x/y/Clazz.java");
+		assertPath("", "a/b/c", "", "");
+		assertPath("", "a/b/c", "", null);
+
+		// current folder
+		boolean isJava = giis.portable.util.Parameters.isJava();
+		assertPath(isJava ? full("x/y/Clazz.java") : "./x/y/Clazz.java", ".", "", "x/y/Clazz.java");
+		assertPath(isJava ? full("x/y/Clazz.java") : "./x/y/Clazz.java", "./", "", "x/y/Clazz.java");
+	}
+	@Test
+	public void testSourcePathResolutionWithProject() {
+		assertPath("/a/b/c/y/Clazz.java", "/a/b/c", "/x", "/x/y/Clazz.java");
+		assertPath("/a/b/c/y/Clazz.java", "/a/b/c", "/x/", "/x/y/Clazz.java");
+		assertPath("/a/b/c/y/Clazz.java", "/a/b/c", "/x", "C:/x/y/Clazz.java");
+		assertPath("/a/b/c/y/Clazz.java", "/a/b/c", "/x/", "C:/x/y/Clazz.java");
+	}
+	
+	public String resolveSourcePath(String sourceFolder, String projectFolder, String sourceFile) {
+		sourceFolder = JavaCs.isEmpty(sourceFolder) ? "" : sourceFolder.trim();
+		projectFolder = JavaCs.isEmpty(projectFolder) ? "" : projectFolder.trim();
+		sourceFile = JavaCs.isEmpty(sourceFile) ? "" : sourceFile.trim();
+		
+		// Source folder and source file are required, if not, the empty return means that source can't be fount
+		if ("".equals(sourceFolder) || "".equals(sourceFile))
+			return ""; 
+		
+		// If projectFolder specified (for net generated coverage), it is expected a full path source File
+		// that is converted to relative to projectFolder
+		if (!"".equals(projectFolder)) {
+			sourceFile = FileUtil.getFullPath(sourceFile).replace("\\", "/");
+			String prefix = FileUtil.getFullPath(projectFolder).replace("\\", "/");
+			if (!prefix.endsWith("/"))
+				prefix = prefix + "/";
+			System.out.println(prefix);
+			System.out.println(sourceFile);
+			if (sourceFile.startsWith(prefix)) {
+				sourceFile = JavaCs.substring(sourceFile, prefix.length(), sourceFile.length());
+			}
+		}
+		return FileUtil.getPath(sourceFolder, sourceFile);
+	}
+
+	private String full(String filename) {
+		return FileUtil.getFullPath(filename).replace("\\", "/");
+	}
+	private void assertPath(String expected, String source, String project, String file) {
+		assertEquals(expected, resolveSourcePath(source, project, file).replace("\\", "/"));
+	}
+}
